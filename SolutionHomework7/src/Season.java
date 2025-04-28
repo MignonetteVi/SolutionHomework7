3cb163d5240f57a26263f1a985d98e67a74fd5c5
Kristina <tolstovachristina@gmail.com>
2025-04-28 10:40:06 +0500

    update Season and SeasonIterator

--- SolutionHomework7/src/Season.java
+++ SolutionHomework7/src/Season.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-public class Season implements Iterable<Episode>{
+public class Season implements Iterable<Episode> {
+    private final List<Episode> episodes = new ArrayList<>();
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    List<Episode> getEpisodes() {
+        return episodes;
+    }
+    
     @Override
     public Iterator<Episode> iterator() {
-        return null;
+        return new SeasonIterator(this);
     }
 }
--- SolutionHomework7/src/SeasonIterator.java
+++ SolutionHomework7/src/SeasonIterator.java
@@ -1,6 +1,10 @@
 import java.util.Iterator;
 
 public class SeasonIterator implements Iterator<Episode> {
+    public SeasonIterator(Season episodes) {
+
+    }
+
     @Override
     public boolean hasNext() {
         return false;
