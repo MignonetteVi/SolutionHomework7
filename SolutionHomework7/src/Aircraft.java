f018f354d75a35af58525d330c92cd52c5c309cc
Kristina <tolstovachristina@gmail.com>
2025-04-28 14:18:28 +0500

    Update Aircraft.java

--- SolutionHomework7/src/Aircraft.java
+++ SolutionHomework7/src/Aircraft.java
@@ -1,2 +1,30 @@
-public interface Aircraft {
+public abstract class Aircraft {
+    protected final String id;
+    protected int fuelLevel;
+
+    protected final TowerMediator tower;
+
+    public Aircraft(String id, int fuelLevel, TowerMediator tower) {
+        this.id = id;
+        this.fuelLevel = fuelLevel;
+        this.tower = tower;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+
+
+    public abstract void receive(String msg);
+
+    public void send(String msg) {
+    }
+
+    public boolean requestRunway() {
+        return true;
+    }
 }
