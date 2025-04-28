5d60d8a47e20ba28ecf78b4bc56cb3bbdb2d2c95
Kristina <tolstovachristina@gmail.com>
2025-04-28 10:45:22 +0500

    Update ReverseSeasonIterator.java

--- SolutionHomework7/src/ReverseSeasonIterator.java
+++ SolutionHomework7/src/ReverseSeasonIterator.java
@@ -1,2 +1,19 @@
-public class ReverseSeasonIterator {
+public class ReverseSeasonIterator implements EpisodeIterator {
+    private final Season season;
+    private int index;
+
+    public ReverseSeasonIterator(Season season) {
+        this.season = season;
+        this.index = season.getEpisodes().size() - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index >= 0;
+    }
+
+    @Override
+    public Episode next() {
+        return season.getEpisodes().get(index--);
+    }
 }
