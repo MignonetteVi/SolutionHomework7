e2f02138e94beb18815da14b50d64219c41632ff
Kristina <tolstovachristina@gmail.com>
2025-04-28 11:25:05 +0500

    Update ShuffleSeasonIterator.java

--- SolutionHomework7/src/ShuffleSeasonIterator.java
+++ SolutionHomework7/src/ShuffleSeasonIterator.java
@@ -1,2 +1,24 @@
-public class ShuffleSeasonIterator {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ShuffleSeasonIterator implements EpisodeIterator {
+    private final List<Episode> shuffled;
+    private int index = 0;
+
+    public ShuffleSeasonIterator(Season season) {
+        this.shuffled = new ArrayList<>(season.getEpisodes());
+        Collections.shuffle(this.shuffled, new Random(42)); // fixed seed for repeatability
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < shuffled.size();
+    }
+
+    @Override
+    public Episode next() {
+        return shuffled.get(index++);
+    }
 }
