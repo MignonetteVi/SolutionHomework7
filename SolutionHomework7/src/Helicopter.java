adf7e395651ce75243ac1d083cfb9495f4e43382
Kristina <tolstovachristina@gmail.com>
2025-04-29 21:21:47 +0500

    Update Helicopter.java

--- SolutionHomework7/src/Helicopter.java
+++ SolutionHomework7/src/Helicopter.java
@@ -1,2 +1,10 @@
-public class Helicopter {
+public class Helicopter extends Aircraft {
+    public Helicopter(String id, int fuelLevel, TowerMediator tower) {
+        super(id, fuelLevel, tower);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.printf("Helicopter %s receives: %s%n", id, msg);
+    }
 }
