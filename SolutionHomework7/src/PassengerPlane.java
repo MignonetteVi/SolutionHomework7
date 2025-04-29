b403a971b780869a98f0d3bbd613695d4713f4f9
Kristina <tolstovachristina@gmail.com>
2025-04-29 21:15:17 +0500

    Update PassengerPlane.java

--- SolutionHomework7/src/PassengerPlane.java
+++ SolutionHomework7/src/PassengerPlane.java
@@ -1,2 +1,10 @@
-public class PassengerPlane {
+public class PassengerPlane extends Aircraft {
+    public PassengerPlane(String id, int fuelLevel, TowerMediator tower) {
+        super(id, fuelLevel, tower);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.printf("PassengerPlane %s receives: %s%n", id, msg);
+    }
 }
