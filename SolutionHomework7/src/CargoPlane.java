3cb6c1150e1fdd02081a49e55eed21130b06321f
Kristina <tolstovachristina@gmail.com>
2025-04-29 21:21:45 +0500

    Update CargoPlane.java

--- SolutionHomework7/src/CargoPlane.java
+++ SolutionHomework7/src/CargoPlane.java
@@ -1,2 +1,10 @@
-public class CargoPlane {
+public class CargoPlane extends Aircraft {
+    public CargoPlane(String id, int fuelLevel, TowerMediator tower) {
+        super(id, fuelLevel, tower);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.printf("CargoPlane %s receives: %s%n", id, msg);
+    }
 }
