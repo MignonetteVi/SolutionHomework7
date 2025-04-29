176da5b2e46cf2666f7e4f333e9ae9577b357000
Kristina <tolstovachristina@gmail.com>
2025-04-29 20:55:21 +0500

    Update ControlTower.java

--- SolutionHomework7/src/ControlTower.java
+++ SolutionHomework7/src/ControlTower.java
@@ -1,2 +1,17 @@
-public class ControlTower {
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class ControlTower implements TowerMediator {
+    private final Queue<Aircraft> landingQueue = new ArrayDeque<>();
+    private final Queue<Aircraft> takeoffQueue = new ArrayDeque<>();
+
+    @Override
+    public void broadcast(String msg, Aircraft sender) {
+
+    }
+
+    @Override
+    public boolean requestRunway(Aircraft a) {
+        return false;
+    }
 }
